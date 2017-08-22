d89e94a1659056344b096b329700023db870fda7
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-08-22 11:56:14 +0300

    Modify styles.css Modify index.html to include tabs for the .foods section Modify scripts.js to alert when user enters email

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-$(document).ready(function(event) {
-  $(".menu").click(function() {
+$(document).ready(function() {
+  $(".menu").click(function(event) {
     $(".menulist").toggle();
-    //$(".walrus-hidden").toggle();
-
     event.preventDefault();
   });
+
+  $("form#subscribe_form").submit(function(event) {
+    event.preventDefault();
+    var emailAddress = $("#subscribe_form_input").val();
+    var notification = emailAddress + " has been successfully added to our email list. Thank you.";
+    alert(notification);
+  });
+
 });
-//  $(".scroll").click(function(event){
-//    event.preventDefault();
-  //  $('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+/*
+$(document).ready(function() {
+  $("form#subscribe_form").submit(function(event) {
+    event.preventDefault();
+    var emailAddress = $(#subscribe_form).val();
+    var notification = emailAddress + " has been successfully added to our email list. Thank you.";
+    alert(notification);
+  });
+
+});*/
